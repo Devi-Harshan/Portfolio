2340f3e9bcd0995ba5657ab7d92949799a637c1f
Devi-Harshan <deviharshan97@gmail.com>
2024-02-01 12:42:29 +0530

    title updated

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,25 +1,25 @@
-import Image from 'next/image'
-import './styles.css'
+import Image from "next/image";
+import "./styles.css";
 
-export default function Dashboard(){
-    return (
-        <div className="hero">
-            <div className="nameWrapper"> Devi Harshan</div>
-            <div className="row">
-   {/*  <div className="col-md-12 text-center">
+export default function Dashboard() {
+  return (
+    <div className="hero">
+      <div className="nameWrapper"> Devi Harshan</div>
+      <div className="row">
+        {/*  <div className="col-md-12 text-center">
       <h3 className="animate-charcter">Front End Developer</h3>
     </div> */}
-    <div className="word">
-  <span>Front End Developer</span>
-</div>
-    <div className="cube"></div>
-  <div className="cube"></div>
-  <div className="cube"></div>
-  <div className="cube"></div>
-  <div className="cube"></div>
-  <div className="cube"></div>
-  </div>
-       {/* <Image
+        <div className="word">
+          <span>Front End Developer</span>
+        </div>
+        <div className="cube"></div>
+        <div className="cube"></div>
+        <div className="cube"></div>
+        <div className="cube"></div>
+        <div className="cube"></div>
+        <div className="cube"></div>
+      </div>
+      {/* <Image
               src="/backgroundGIF.gif"
               alt="Gif"
               className="dark:invert"
@@ -27,6 +27,6 @@ export default function Dashboard(){
               layout='fill'
               priority
             />  */}
-        </div>
-    )
+    </div>
+  );
 }
