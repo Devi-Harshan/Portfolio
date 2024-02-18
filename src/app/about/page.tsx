f38e511aa09524c19aa6ace353787d172ec9f951
Devi-Harshan <deviharshan97@gmail.com>
2024-02-18 22:35:54 +0530

    build error resolved

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 export default function About() {
   return (
@@ -12,7 +13,7 @@ export default function About() {
         <div className="container mx-auto flex px-10 py-10 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-              Hi, I'm Devi Harshan.
+              {"Hi, I'm Devi Harshan."}
               {/*  <br className="hidden lg:inline-block" />I love to build amazing
             apps. */}
             </h1>
@@ -37,11 +38,13 @@ export default function About() {
             </div>{" "}
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img
+            <Image
               className="object-cover object-center rounded"
               alt="hero"
+              width={400}
+              height={400}
               style={{ borderRadius: "50%" }}
-              src="./skills.png"
+              src="/skills.png"
             />
           </div>
         </div>
